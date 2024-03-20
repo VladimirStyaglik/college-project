b6d296350c7e0a1247d317c3c6ccddf3070fd31e
Vova Styaglyk <vvstyaglik@gmail.com>
2024-03-20 08:34:23 +0200

    Add implementions to GroupEntity, GroupManagerEntity, PersonEntity, StudentEntity, TeacherEntity, SubjectEntity.

--- Groups/Implementions/StudentEntity.cs
+++ Groups/Implementions/StudentEntity.cs
@@ -2,5 +2,14 @@ namespace Groups.Implementions;
 
 public class StudentEntity : PersonEntity
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
 
+    public StudentEntity(int id, string name, string email) : base(id, name, email)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+    }
 }
